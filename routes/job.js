9e2bea53cca7ef8f1354b8cb57220486497a53c4
engWafula <wafulaallan5@gmail.com>
2023-08-20 11:59:03 +0300

    adding new changes

--- routes/job.js
+++ routes/job.js
@@ -8,8 +8,8 @@ const router = express.Router()
 
 
 router.post("/",isAuth,feedController.createJobs)
-router.get("/",isAuth,feedController.getJobs)
-router.get("/:id",isAuth,feedController.getJob)
+router.get("/",feedController.getJobs)
+router.get("/:id",feedController.getJob)
 router.delete("/:id",isAuth,feedController.deleteJob)
 router.patch("/:id",isAuth,feedController.updateJob)
 
